3fffe1feda07b68fb61c767c7565a6a703b68f34
webx-downloader <166244237+webx-downloader@users.noreply.github.com>
2024-06-21 22:06:23 +0200

    Add files via upload

---
+++ ddos1.js
@@ -0,0 +1,46 @@
+//WEB URL!!!!!!!//
+console.log(location.href);
+//DEV : PASSWORD//
+console.log("password:downloadwebxnow")
+//GOMB A HTMLBE IS ÉS ÉRTESÍTÉS COMMAND//
+var gomb = document.getElementById("STARTUP");
+
+gomb.addEventListener("mousedown", function(e){
+    if (e.button == 2) { // jobb kattintás
+        e.preventDefault();
+     // eltávolítja az árnyékot, ami a lenyomott hatást kelti
+    }
+});
+function bestFunction() {
+    let password;
+    if (confirm("passwordRequired") == true ) {
+      var x =document.createElement("INPUT");
+      /* < a textbox elhelyezkedése >*/ 
+      x.style.position = 'absolute';
+      x.style.left = '150px';
+      x.style.top = '335px';
+      /*<--------------------------->*/
+      x.setAttribute("type" , "text");
+      document.body.appendChild(x)
+      const button = document.getElementById("STARTUP");
+      button.disabled = true;
+      
+    } else {
+       alert("ACCESS DENIED")
+    }
+   
+  }
+
+
+
+   // kis kiegészítés hogy ne lehessen kijelölni semmit //
+   document.addEventLis0tener('dragstart', function (e) {
+    e.preventDefault();
+});
+document.addEventLis0tener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+
+
+
